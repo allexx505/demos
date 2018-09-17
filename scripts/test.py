78de9bed50e04d0845968cd5640a248a08c0bf3b
alextan <allexx505@yahoo.com>
2018-09-17 23:03:56 +0800

    提交语法糖尝试代码

--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,19 @@
 # @Author  : Alex-tan
 # @FileName: test.py
 
+OPEN_JOB_FLOW = False
+
+def switch_on_required(func):
+    def _check_open(a,b,c):
+        global OPEN_JOB_FLOW
+        if OPEN_JOB_FLOW:
+            ret = func(a,b,c)
+            return ret
+        else:
+            print('switch is off!!please open first')
+            return False
+    return _check_open
+
 
 class ExecuteByStep(object):
     def __init__(self,info):
@@ -26,11 +39,14 @@ class ExecuteByStep(object):
 def chengfa(x,y):
     return x*y
 
+@switch_on_required
 def calculator(x,y,suanfa):
     return suanfa(x,y)
 
 if __name__ == '__main__':
-    print(calculator(2,2,chengfa))
+    # OPEN_JOB_FLOW = True
+    a = calculator(2,2,chengfa)
+    print(a)
     # todolist = [['buy', 1], ['cut', 3], ['cook', 5], ['eat', 2], ['wash', 9], ['play', 7], ['sleep', 3]]
     # todoobj = ExecuteByStep(todolist)
     # todoobj.execute()
