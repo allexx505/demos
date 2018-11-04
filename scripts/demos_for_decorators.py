9007abb9bcc5e5bad28de1b4c9129e2f9bc49c88
alextan <allexx505@yahoo.com>
2018-11-04 17:03:56 +0800

    commit some test codes

--- scripts/demos_for_decorators.py
+++ scripts/demos_for_decorators.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 # @Create Time : 2018/9/11 22:13
 # @Author  : Alex-tan
-# @FileName: test.py
+# @FileName: demos_for_decorators.py
 
 OPEN_JOB_FLOW = False
 
+
 def switch_on_required(func):
-    def _check_open(a,b,c):
+    def _check_open(a, b, c):
         global OPEN_JOB_FLOW
         if OPEN_JOB_FLOW:
-            ret = func(a,b,c)
+            ret = func(a, b, c)
             return ret
         else:
             print('switch is off!!please open first')
@@ -18,7 +19,7 @@ def switch_on_required(func):
 
 
 class ExecuteByStep(object):
-    def __init__(self,info):
+    def __init__(self, info):
         self.index = 0
         self.next_step = self.index + 1
         self.execute_next = False
@@ -28,24 +29,24 @@ class ExecuteByStep(object):
         if self.index == len(self.todolist):
             print('finish!!!!!!!!!!!')
         else:
-            print('now I am doing the step:',self.todolist[self.index])
+            print('now I am doing the step:', self.todolist[self.index])
             self.index = self.next_step
             self.next_step += 1
             self.execute()
 
 
+def chengfa(x, y):
+    return x * y
 
-
-def chengfa(x,y):
-    return x*y
 
 @switch_on_required
-def calculator(x,y,suanfa):
-    return suanfa(x,y)
+def calculator(x, y, suanfa):
+    return suanfa(x, y)
+
 
 if __name__ == '__main__':
-    # OPEN_JOB_FLOW = True
-    a = calculator(2,2,chengfa)
+    OPEN_JOB_FLOW = True
+    a = calculator(2, 2, chengfa)
     print(a)
     # todolist = [['buy', 1], ['cut', 3], ['cook', 5], ['eat', 2], ['wash', 9], ['play', 7], ['sleep', 3]]
     # todoobj = ExecuteByStep(todolist)
---
+++ scripts/function_code.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Create Time : 2018/9/22 18:33
+# @Author  : Alex-tan
+# @FileName: function_code.py
+
+from functools import *
+print(reduce(lambda x, y: x + y, [1, 2, 3, 4, 5]))
