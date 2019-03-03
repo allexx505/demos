e75fd6f15f044b0b43389d8382cdfde567a084c7
alextan <allexx505@yahoo.com>
2019-03-03 10:36:36 +0800

    curl test code

---
+++ scripts/curl_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Create Time : 2019/3/1 21:28
+# @Author  : Alex-tan
+# @FileName: curl_test.py
+
+import os
+import sys
+
+
+if __name__ == '__main__':
+    os.system('curl')
