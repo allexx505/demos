b049a7c2dff14bae516e3784a05ade669079a617
alextan <allexx505@yahoo.com>
2019-03-10 23:49:22 +0800

    django course ending code

--- django_demos/user_mgr/models.py
+++ django_demos/user_mgr/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import  User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    '''
+    '''
+    title = models.CharField(max_length=80)
+    subtitle = models.CharField(max_length=120)
+    publish_date = models.DateTimeField()
+    content = models.TextField()
+    link = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
+
--- django_demos/user_mgr/views.py
+++ django_demos/user_mgr/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
     word_list = ['1', '2', '3']
+    word_list = Post.objects.all().order_by('-publish_date')
     return render(request, 'index.html',
                   {"index_title": "django_test website",
                    "word_list": word_list})
